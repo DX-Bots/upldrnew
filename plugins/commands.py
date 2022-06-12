377228a56696f84251377c614e6e56ccfc181085
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-06-12 10:10:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,4 +41,53 @@ async def start(bot, update):
         reply_markup=Translation.START_BUTTONS
     )
 
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help(bot, update):
+    if not update.from_user:
+        return await update.reply_text("Who the Hell Are You?")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+    await bot.send_chat_action(
+       chat_id=update.chat.id,
+       action="typing"
+    )
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
+        text=Translation.HELP_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.HELP_BUTTONS
+    )
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(bot, update):
+    if not update.from_user:
+        return await update.reply_text("Who the Hell Are You?")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+    await bot.send_chat_action(
+       chat_id=update.chat.id,
+       action="typing"
+    )
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
+        text=Translation.ABOUT_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.ABOUT_BUTTONS
+    )
+
+
+
+
 
